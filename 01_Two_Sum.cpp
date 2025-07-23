09e8945695aca81887a28e93c973034dfedc6b0a
Rahul Kumar <162640729+Rahul123ab@users.noreply.github.com>
2025-07-22 23:09:05 -0400

    Add files via upload

---
+++ 01_Two_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+    vector<int> twoSum(vector<int> &nums, int target){//TC= O(n)
+
+        unordered_map<int, int> map;
+
+        for(int i=0; i<nums.size(); i++) {
+            int first = nums[i]; //fix the first element in every iteration
+            int second = target - first; //calculate the second element or complement to achieve target
+
+            if(map.find(second) != map.end()){ //check if second value already exists in the map
+                return {i,map[second]};
+            }
+            map[nums[i]]=i; //just add the current array element to the map
+        }
+        return {};
+    }
+};
