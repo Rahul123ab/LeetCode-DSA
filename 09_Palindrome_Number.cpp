20b903951f0ca0cda880d25ed65c5b375e446e15
Rahul Kumar <rahul.wmu@gmail.com>
2025-08-17 16:01:23 -0400

    Update 09_Palindrome_Number.cpp

--- 09_Palindrome_Number.cpp
+++ 09_Palindrome_Number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
         long rev = 0;
         int n = x;
 
         while(n > 0) {
-            
-            int remainder = n%10;
-            rev = rev*10 + remainder;
+            int rem = n % 10;
+            rev = rev * 10 + rem;
             n = n/10;
         }
-
-        if(rev == x)
+        if (rev == x) {
             return true;
+        } 
         return false;
     }
 };
